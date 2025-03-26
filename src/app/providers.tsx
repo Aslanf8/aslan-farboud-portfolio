81e6683f7813c7ba20d414659d846c437ba7a6a1
Aslan <aslan.farboud@gmail.com>
2025-03-26 23:37:51 +0000

    added all bloglogic

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,10 +4,10 @@ import { SWRConfig } from "swr";
 import { ReactNode, useEffect, useState } from "react";
 
 // Define custom error type
-interface FetchError extends Error {
-  info?: unknown;
-  status?: number;
-}
+// interface FetchError extends Error {
+//   info?: unknown;
+//   status?: number;
+// }
 
 // Global fetcher function for SWR
 const fetcher = async (url: string) => {
@@ -16,10 +16,10 @@ const fetcher = async (url: string) => {
   // If the status code is not in the range 200-299,
   // we still try to parse and throw it.
   if (!res.ok) {
-    const error = new FetchError("An error occurred while fetching the data.");
+    const error = new Error("An error occurred while fetching the data.");
     // Attach extra info to the error object.
-    error.info = await res.json();
-    error.status = res.status;
+    // error.info = await res.json();
+    // error.status = res.status;
     throw error;
   }
 
